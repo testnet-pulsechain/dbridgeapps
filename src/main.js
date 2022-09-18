e5c4befcaec853cce477ec782830ff63ad8abd35
blawlidris <blawlidris@gmail.com>
2022-09-18 10:30:05 +0100

    _redirect routing

--- src/main.js
+++ src/main.js
@@ -6,4 +6,4 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
